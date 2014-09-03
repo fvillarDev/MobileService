5b357f00dfcf4f908115a798c3c57699ed9a05f6
fv_android <facuandroid.fv@gmail.com>
2014-09-03 15:19:12 -0300

    Add Get Position By Stop

--- HorariosService/ServiceMobile.asmx.cs
+++ HorariosService/ServiceMobile.asmx.cs
@@ -26,12 +26,6 @@ namespace MobileService
         string BASE_URL = "http://ws_geosolution.geosolution.com.ar/mobile_test/Mobile/";
 
         [WebMethod]
-        public string TestService()
-        {
-            return "OK";
-        }
-
-        [WebMethod]
         public string GetHourMobile(string stop, string bus)
         {
             bool isNoData = true;
@@ -68,6 +62,26 @@ namespace MobileService
             }
             return res;
         }
+
+        [WebMethod]
+        public string GetPositionByStop(string stop)
+        {
+            bool isNoData = true;
+            string res = "";
+            while (isNoData)
+            {
+                res = GetParadaStop(stop);
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(res);
+                if (!doc.DocumentElement.FirstChild.Value.Contains("Error"))
+                {
+                    isNoData = false;
+                    res = doc.DocumentElement.FirstChild.Value.ToString();
+                }
+            }
+            return res;
+        }
+
 
         private string GetData(string parada, string linea)
         {
@@ -114,6 +128,32 @@ namespace MobileService
             string uri = BASE_URL + "ObtenerParadasCercanasPorLinea?" +
                          "linea=" + linea + 
                          "&longitud=" + lng + "&latitud=" + lat + 
+                         "&id=" + idGeo +
+                         "&hash=" + hashBuild + "&fecha=" + dayToSend;
+
+            WebRequest req = WebRequest.Create(uri);
+            WebResponse resp = req.GetResponse();
+
+            StreamReader sr = new StreamReader(resp.GetResponseStream());
+            return sr.ReadToEnd().Trim();
+        }
+
+        private string GetParadaStop(string parada)
+        { 
+            DateTime date = DateTime.Now;
+            string dayToSend = date.ToString("yyyy-MM-ddThh:mm:ss");
+            string mensajeFecha = date.ToString("yyyyMMddhhmmss");
+            string hashBuild = "";
+            byte[] keyArray;
+            using (HMACMD5 m = new HMACMD5(UTF8Encoding.UTF8.GetBytes(key)))
+            {
+                keyArray = m.ComputeHash(UTF8Encoding.UTF8.GetBytes(mensajeFecha));
+            }
+
+            hashBuild = Convert.ToBase64String(keyArray, 0, keyArray.Length);
+
+            string uri = BASE_URL + "ObtenerParada?" +
+                         "parada=" + parada +
                          "&id=" + idGeo +
                          "&hash=" + hashBuild + "&fecha=" + dayToSend;
 
